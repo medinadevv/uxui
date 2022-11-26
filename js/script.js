d2fa2db0ef20e38e379ea8422df464890fdf0250
medinadevv <gulzina082@gmai.com>
2022-11-26 14:23:26 +0600

    uxui

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
